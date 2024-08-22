b8cb5ac234cf10b7d2430269e0dee41cbdca2a6e
来铧敏 <laihuamin@Mac-Pro.local>
2024-08-22 08:21:56 +0800

    feat: 读取命令行

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 // $ cargo run searchstring example-filename.txt
 // 任务目标可以读取参数
+// 第一步：引入io，可以读写参数
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    // 读取run参数
+    // Vec是个什么东西
+    // 类型定义都是大写的
+    let args: Vec<String> = env::args().collect();
+    
+    println!("{:?}", args);
 }
